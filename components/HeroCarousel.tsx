4e51b5999aecc840e96b42525efae150df0148fa
Soumik Sil <soumiksilco@gmail.com>
2025-10-21 23:25:13 +0530

    added new images for the hero section

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -8,6 +8,11 @@ import image3 from "../assets/3.jpg";
 import image4 from "../assets/4.jpg";
 import image5 from "../assets/5.jpg";
 import image6 from "../assets/6.jpg";
+import image7 from "../assets/7.jpg";
+import image8 from "../assets/8.jpg";
+import image9 from "../assets/9.jpg";
+import image10 from "../assets/10.jpg";
+import image11 from "../assets/11.jpg";
 const images = [
   image1,
   image2,
@@ -15,6 +20,11 @@ const images = [
   image4,
   image5,
   image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
 ]
 
 export function HeroCarousel() {
